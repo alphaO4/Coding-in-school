50454e60c01a5e3515984c5d2fbda2ca3b1be177
AlphaO4 <jonah.svoboda.2004@gmail.com>
2020-11-16 13:43:55 +0100

    8-LEDS.ino added

---
+++ Code/Arduino/8-LEDS.ino
@@ -0,0 +1,25 @@
+int LED1=11;
+int LED2=10;
+int LED3=9;
+int LED4=8;
+int LED5=7;
+int LED6=6;
+int LED7=5;
+int LED8=4;
+int Eingang=A0;
+int Sensorwert=0;
+int dwiscool=0;
+int helligkeit=0;
+
+void setup(){
+pinMode(LED1, OUTPUT)
+pinMode(LED2, OUTPUT)
+pinMode(LED3, OUTPUT)
+pinMode(LED4, OUTPUT)
+pinMode(LED5, OUTPUT)
+pinMode(LED6, OUTPUT)
+pinMode(LED7, OUTPUT)
+pinMode(LED8, OUTPUT)
+}
+
+void loop(){
