2f9df6ff53536213ac6371a6d3250c0a5e96ea59
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2021-03-16 12:46:21 +0100

    ITS FUCKING WORKING

--- Code/Corona_ampel/Requests_test.py
+++ Code/Corona_ampel/Requests_test.py
@@ -1,53 +1,63 @@
 import json
 import requests
-from datetime import date, timedelta         #
+from datetime import date, timedelta         #Time is important
 from operator import index
-#import PySimpleGUI as sg
+import PySimpleGUI as sg
 
 # Get the date from yesterday, so the printout is more organiesd even after a long time of running.
 yesterday = date.today() - timedelta(days=1)
-spl_word = yesterday                         #
+spl_word = yesterday                        #
 print("Coronazahlen vom: " + str(spl_word))
+print("\n")
 
 # get the API data.
 r = requests.get('https://www.berlin.de/lageso/gesundheit/infektionsepidemiologie-infektionsschutz/corona/tabelle-indikatoren-gesamtuebersicht/index.php/index/today.json')
 
-print(r.json()['index'][0])  # print the latest info in the terminal.
-print("")
+print(r.json()['index'][0],"\n")  # print the latest info in the terminal.
+#print("")
 
-print("7_tage_inzidenz:")
-# 7 tage inzidenz amfragen
+# 7 tage inzidenz abfragen
 Daten = float(r.json()['index'][0]['7_tage_inzidenz'])
 if Daten >= 30:  # wenn über 30
-    print("ROT")
+    print("7_tage_inzidenz - ", float(r.json()['index'][0]['7_tage_inzidenz']), " - ROT","\n")
 elif 20 <= Daten <= 30:  # wenn unter 30
-    print("GELB")
+    print("7_tage_inzidenz - ", float(r.json()['index'][0]['7_tage_inzidenz']), " - GELB","\n")
 elif Daten <= 19:  # wenn unter 20
-    print("GRÜN")
+    print("7_tage_inzidenz - ", float(r.json()['index'][0]['7_tage_inzidenz']), " - GRÜN","\n")
 
-print("its_belegung:")
+
+
+#its_belegung
 Daten = float(r.json()['index'][0]['its_belegung'])
 if Daten >= 25:
-    print("ROT")
+    print("its_belegung - ", float(r.json()['index'][0]['7_tage_inzidenz']), " - ROT","\n")
 elif 15 <= Daten <= 25:
-    print("GELB")
+    print("its_belegung - ", float(r.json()['index'][0]['7_tage_inzidenz']), " - GELB","\n")
 elif Daten <= 14:
-    print("GRÜN")
+    print("its_belegung - ", float(r.json()['index'][0]['7_tage_inzidenz']), " - GRÜN","\n")
 
-print("4_tage_r_wert_berlin_rki:")
 
-p = 0
-while float(r.json()['index'][p]['4_tage_r_wert_berlin_rki']) == 0:
-    p + 1
-if float(r.json()['index'][p]['4_tage_r_wert_berlin_rki']) != 0:
-    Daten = float(r.json()['index'][0]['4_tage_r_wert_berlin_rki'])
+
+#R-wert von letzter veränderung
+index = 1
+while float(r.json()['index'][index]['4_tage_r_wert_berlin_rki']) == 0:
+    index = index + 1
+if float(r.json()['index'][index]['4_tage_r_wert_berlin_rki']) != 0:
+    Daten = float(r.json()['index'][index]['4_tage_r_wert_berlin_rki'])
     if Daten >= 1.2:
-        print("ROT")
+        print("4_tage_r_wert_berlin_rki - ", float(r.json()['index'][index]['4_tage_r_wert_berlin_rki']), " - ROT","\n")
     elif 1.1 <= Daten <= 1.2:
-        print("GELB")
+        print("4_tage_r_wert_berlin_rki - ", float(r.json()['index'][index]['4_tage_r_wert_berlin_rki']), " - GELB","\n")
     elif Daten <= 1.0:
-        print("GRÜN")   
-   
+        print("4_tage_r_wert_berlin_rki - ", float(r.json()['index'][index]['4_tage_r_wert_berlin_rki']), " - GRÜN","\n")
+
+
+
+
+
+
+
+
 
 
 # while RWert == 0:
