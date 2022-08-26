b8441da52c50f9d9065d41d7e3a705bb1c25e992
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2022-08-26 22:34:32 +0200

    Small update

--- Code/Arduino/servo_Ansteuern/servo_Ansteuern.ino
+++ Code/Arduino/servo_Ansteuern/servo_Ansteuern.ino
@@ -1,13 +1,15 @@
 #include <Servo.h>
 Servo myservo;
-int pot = 0; 
+int pot = 0;
 int value;
-void setup() {
+void setup()
+{
     myservo.attach(9);
 }
-void loop() {
+void loop()
+{
     value = analogRead(pot);
-    value = map (value, 0, 1023, 0, 180);
+    value = map(value, 0, 1023, 0, 180);
     myservo.write(value);
     delay(15);
 }
